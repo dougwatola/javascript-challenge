5c1809d349b2f628ec5a685de01c857d98bda9f2
Doug Watola <dougwatola@gmail.com>
2020-01-02 16:58:14 -0500

    Added test files

--- UFO-level-1/app.js
+++ UFO-level-1/app.js
@@ -1,21 +1,21 @@
-// from data.js
+// load data from data.js into variable
 var tableData = data;
 
-// get table references
+// get table body reference
 var tbody = d3.select("tbody");
 
 function buildTable(data) {
-  // First, clear out any existing data
+  // Initialize or make sure the table is empty
   tbody.html("");
 
-  // Next, loop through each object in the data
-  // and append a row and cells for each value in the row
+  // Iterate through each object in data and 
+  // append the table appropriately
+  
   data.forEach((dataRow) => {
     // Append a row to the table body
     var row = tbody.append("tr");
 
-    // Loop through each field in the dataRow and add
-    // each value as a table cell (td)
+    // Add data to each table cell (td) in the row
     Object.values(dataRow).forEach((val) => {
       var cell = row.append("td");
         cell.text(val);
@@ -26,26 +26,22 @@ function buildTable(data) {
 
 function handleClick() {
 
-  // Grab the datetime value from the filter
+  // Get the date from the mouse click
   var date = d3.select("#datetime").property("value");
   let filteredData = tableData;
 
-  // Check to see if a date was entered and filter the
-  // data using that date.
+  // Filter the date information given
   if (date) {
-    // Apply `filter` to the table data to only keep the
-    // rows where the `datetime` value matches the filter value
     filteredData = filteredData.filter(row => row.datetime === date);
   }
 
   // Rebuild the table using the filtered data
-  // @NOTE: If no date was entered, then filteredData will
-  // just be the original tableData.
+  // Display all data if no date was given
   buildTable(filteredData);
 }
 
-// Attach an event to listen for the form button
+// Event for mouseclick
 d3.selectAll("#filter-btn").on("click", handleClick);
 
-// Build the table when the page loads
+// Build the table 
 buildTable(tableData);
